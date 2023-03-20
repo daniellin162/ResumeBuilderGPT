8de9f438247932e03c5f70b1ac2470221b8f2433
dllin <dllin@ucsd.edu>
2023-03-20 02:17:21 -0700

    Add react to print library

--- resume-builder/client/src/App.js
+++ resume-builder/client/src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
+
+    const [result, setResult] = useState({});
+
     return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/resume' element={<Resume />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home setResult={setResult} />} />
+					<Route path='/resume' element={<Resume result={result} />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
